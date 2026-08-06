d90cebda122fb274f8a62f741e271ce30a11c0c6
Raghu Kumar Gadepalli <raghu.gadepalli@gmail.com>
2026-08-06 09:31:24 +0530

    Refill

--- tests/replays/replay_snapshots.py
+++ tests/replays/replay_snapshots.py
@@ -24,7 +24,7 @@ from services.snapshot.snapshot_generator import SnapshotGenerator
 # ---------------------------------------------------------------------------
 # Hard-coded replay configuration
 # ---------------------------------------------------------------------------
-START = datetime(2026, 8, 6, 9, 18, tzinfo=ZoneInfo("Asia/Kolkata"))
+START = datetime(2026, 8, 6, 9, 21, tzinfo=ZoneInfo("Asia/Kolkata"))
 END = datetime(2026, 8, 6, 9, 33, tzinfo=ZoneInfo("Asia/Kolkata"))
 
 # Use exactly one of these forms:
